26f7a4bab2cba7865fe74cbb2dd3a86b1cef691a
GURPRIT SINGH <gurprit.iiest@gmail.com>
2022-12-21 15:19:55 +0530

    Create mapValuesToPath.js

---
+++ mapValuesToPath.js
@@ -0,0 +1,29 @@
+// Given a deeply nested js object return the map of the paths to the values
+// e.g obj = { 
+//    a: {
+//       b: 2,
+//       c: 3 
+//    },
+   
+//    d: 4
+// };
+
+// it should return { "a.b": 2, "a.c": 3, "d" : 4 };
+
+
+
+function mapValuesToPath(obj, parentKey = '') { 
+  let result = { };
+  
+  for(let [key, value] of Object.entries(obj) { 
+    const path = parentKey ? `${parentKey}.{key}` : 
+    if(typeof value !== "object" && !Array.isArray(value)) {
+      result[path] = value;
+    }else {
+     result = { ...result, ...mapValuesToPath(value, path) }
+    }
+  
+  } 
+  
+  return result;
+}
